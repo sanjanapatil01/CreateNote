a9613ae62b43d69a7ede560c1d43739250744896
sanjanampatil01 <sanjanampatil6@gmail.com>
2025-09-08 18:33:59 -0600

    Added .env file

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", 
+  // 👆 Use env variable in production, fallback to local
 });
 
 // Add token automatically for protected routes
